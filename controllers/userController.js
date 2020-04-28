bae68d4ea87d1bacfe7066f37977dc63ff3806b6
donghoonlee94 <94donghoon.lee@gmail.com>
2020-04-28 23:13:10 +0900

    search form 추가, 검색 데이터 req.query 로 받아와서 템플릿에 변수로 전달

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,11 @@
 // render 함수의 인자로 파일명을 적어주면 되고, 확장자가 pug인 템플릿 파일을 찾은 후 렌더해준다. app.set('view engine', 'pug')를 설정했기에 가능한 것.
-export const join = (req, res) => res.render("join");
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
-export const userDetail = (req, res) => res.render("userDetail");
-export const editProfile = (req, res) => res.render("editProfile");
-export const changePassword = (req, res) => res.render("changePassword");
+export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const login = (req, res) => res.render("login", { pageTitle: "Log In" });
+export const logout = (req, res) =>
+  res.render("logout", { pageTitle: "Log Out" });
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "User Detail" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "Change Password" });
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,13 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("search");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const editVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+export const home = (req, res) => res.render("home", { pageTitle: 'home' });
+
+export const search = (req, res) => {
+  // 아래 문법이 const searchingBy = req.query.term 와 같음. es6의 문법. 
+  const { query: {term: searchingBy }} = req;
+  // 데이터를 render 시 같이 내려준다. 내려주기 때문에 search.pug 에서 #{searchingBy} 로 노출해줄 수 있음.
+  res.render("search", { pageTitle: 'search', searchingBy});
+}
+
+export const upload = (req, res) => res.render("upload", { pageTitle: 'upload' });
+export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: 'videoDetail' });
+export const editVideo = (req, res) => res.render("editVideo", { pageTitle: 'editVideo' });
+export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: 'deleteVideo' });
