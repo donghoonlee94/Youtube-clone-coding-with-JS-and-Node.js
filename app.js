3e5d055a44eee02072c00a5967c27948b2f1178d
donghoonlee94 <94donghoon.lee@gmail.com>
2020-07-08 22:31:43 +0900

    [feat] Register View Count API

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ import routes from './routes';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
 import globalRouter from './routers/globalRouter';
+import apiRouter from "./routers/apiRouter";
 
 dotenv.config();
 import './passport';
@@ -62,5 +63,6 @@ app.use(localsMiddleware);
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
+app.use(routes.api, apiRouter);
 
 export default app;
